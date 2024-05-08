1f022a96ac3242fbf7151749a9c379c5dad5360b
Aruntgym <arunkumarg7845@gmail.com>
2024-05-08 23:13:01 +0530

    second commit

--- script.js
+++ script.js
@@ -21,14 +21,44 @@ const person = {
 console.log(person.city);
 // Ans: undefined
 
-let rF = 10;
-function check() {
-    console.log(rF);
-    let rF = 20;
-}
-check();
+// let rF = 10;
+// function check() {
+//     console.log(rF);
+//     let rF = 20;
+// }
+// check();
 // Ans : ReferenceError
 
+let xa = 10;
+if(xa === "10") {
+    console.log("Equal");
+}else{
+    console.log("Not Equal");
+}
+// Ans: Not Equal
 
+console.log(2 ** 3);
+// Ans: 8
 
+let x1 = 5;
+let y1 = 0;
+let result;
+if (y1 != 0){
+    result = x1 / y1;
+    console.log(result);
+}else{
+    console.log("Error: Division by zero");
+} 
+// Ans: Error: Division by zero
 
+console.log(2 + "2" - 1);
+// Ans: 21
+
+console.log(0 == "");
+// Ans: true
+
+// function myFunction(){
+//     var x2 = 10;
+// }
+// console.log(x2);
+// Ans: ReferenceError
