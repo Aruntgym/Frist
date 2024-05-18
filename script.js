d16f9e96abb5f0049ff662641a86a373b0d6a895
Aruntgym <arunkumarg7845@gmail.com>
2024-05-18 13:57:55 +0530

    Eigth commit

--- script.js
+++ script.js
@@ -201,3 +201,36 @@ countOccurences('malayalam')
 // occurences of a is :4
 // occurences of l is :2
 // occurences of y is :1
+
+function palindrome(str)
+  {
+    var flag=0;
+     for(var i=0,j=str.length-1;i<(str.length/2)&&j>0; i++,j--)
+     {
+     if(str[i]!=str[j])
+    flag=1;
+    
+  }
+  if(flag==1) {
+     console.log("string "+str+" is not a palindrome");
+         } else {
+            console.log("string "+str+" is a palindrome"); 
+         }
+  }
+
+  palindrome("malayalam"); //string malayalam is a palindrome
+  palindrome("malaam"); //string malaam is not a palindrome
+
+//   function returnMissingNumber(arr) {
+//     let n=arr.length+1;
+//     let sum=0;
+//     for(let item of arr) {
+//       sum+=item;
+//     }
+//     x=(n*(n+1)/2)-sum;
+//     return x;
+//    }
+
+//    let arr=[1,2,3,5];
+//    console.log(returnMissingNumber(arr));  //4
+
